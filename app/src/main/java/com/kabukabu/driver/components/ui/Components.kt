a52075cc6da87a733f32c37b045618d0e3258a4a
dejito <Deerealboy@gmail.com>
2025-10-06 22:39:35 +0100

    text field ui implementation completed

--- app/src/main/java/com/kabukabu/driver/components/ui/Components.kt
+++ app/src/main/java/com/kabukabu/driver/components/ui/Components.kt
@@ -1,16 +1,23 @@
 package com.kabukabu.driver.components.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldColors
@@ -18,6 +25,7 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -25,6 +33,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.kabukabu.driver.R
 import com.kabukabu.driver.components.utils_functions.priceFilter
 
 
@@ -88,6 +97,14 @@ fun KabuOutlinedTextField(
             .focusable(enabled = focusable!!)
             .background(color = Color.Transparent)
     ) {
+        // Base color configuration: everything default, except border/outline is transparent
+        val noBorderColors = textFieldColors.copy(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            errorIndicatorColor = Color.Transparent
+        )
+
         when (keyboardType) {
             "number" -> {
                 OutlinedTextField(
@@ -110,11 +127,12 @@ fun KabuOutlinedTextField(
                             fontSize = 14.sp
                         )
                     },
-                    colors = textFieldColors,
+                    colors = noBorderColors,
                     visualTransformation = {
                         priceFilter(it, isAmount)
                     },
-                    singleLine = true, isError = isValidationError,
+                    singleLine = true,
+                    isError = isValidationError,
                     enabled = isClickable,
                     modifier = Modifier
                         .fillMaxWidth()
@@ -151,13 +169,12 @@ fun KabuOutlinedTextField(
                     modifier = Modifier
                         .fillMaxWidth()
                         .wrapContentHeight(),
-
                     keyboardOptions = KeyboardOptions.Default.copy(
                         keyboardType = KeyboardType.NumberPassword,
                         imeAction = imeAction
                     ),
                     isError = isValidationError,
-                    colors = textFieldColors
+                    colors = noBorderColors
                 )
             }
 
@@ -191,10 +208,74 @@ fun KabuOutlinedTextField(
                         imeAction = imeAction
                     ),
                     isError = isValidationError,
-                    colors = textFieldColors
+                    colors = noBorderColors
                 )
             }
         }
     }
+}
 
+
+@Composable
+fun KabuOutlinedTextFieldWithTrailingIconButton(
+    value: String? = "",
+    placeholderText: String,
+    iconTint: Color = Color.Gray,
+    bottomPadding: Int = 15,
+    onClick: () -> Unit = {},
+    isError: Boolean = false,
+    onTextChanged: (text: String) -> Unit = {},
+    textFieldColors: TextFieldColors = TextFieldDefaults.colors()
+) {
+    // Use base theme but make only the border invisible
+    val noBorderColors = textFieldColors.copy(
+        focusedIndicatorColor = Color.Transparent,
+        unfocusedIndicatorColor = Color.Transparent,
+        disabledIndicatorColor = Color.Transparent,
+        errorIndicatorColor = Color.Transparent,
+        disabledContainerColor = Color(0xFFF1F1F1),
+
+    )
+
+    Box(
+        modifier = Modifier
+            .padding(bottom = bottomPadding.dp)
+            .background(Color.Transparent)
+    ) {
+        OutlinedTextField(
+            value = value ?: "",
+            onValueChange = { newValue ->
+                onTextChanged(newValue)
+            },
+            readOnly = true,
+            enabled = false,
+            isError = isError,
+            colors = noBorderColors,
+            placeholder = {
+                Text(
+                    text = placeholderText,
+                    fontSize = 14.sp
+                )
+            },
+            singleLine = true,
+            shape = RoundedCornerShape(10),
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.chevron_down),
+                    tint = iconTint,
+                    contentDescription = null
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .clickable { onClick() }
+        )
+    }
+}
+
+
+@Composable
+fun KabuSpacer(width: Int) {
+    Spacer(modifier = Modifier.width(width.dp))
 }
--- app/src/main/java/com/kabukabu/driver/features/auth/presentation/sign_up/view/DriverBioDataScreen.kt
+++ app/src/main/java/com/kabukabu/driver/features/auth/presentation/sign_up/view/DriverBioDataScreen.kt
@@ -4,11 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,6 +17,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.kabukabu.driver.components.ui.KabuDivider
 import com.kabukabu.driver.components.ui.KabuOutlinedTextField
+import com.kabukabu.driver.components.ui.KabuOutlinedTextFieldWithTrailingIconButton
+import com.kabukabu.driver.components.ui.KabuSpacer
 import com.kabukabu.driver.components.ui.TitleText
 
 @Composable
@@ -39,17 +42,43 @@ fun DriverBioDataScreen() {
                 bottomPadding = 12,
             )
 
-            Box(
-                modifier = Modifier
-                    .background(color = Color(0x2DF1F1F1))
-                    .padding(vertical = 12.dp)
-                    .fillMaxWidth()
-            ) {
-                AnnotatedTextfieldTitle(title = "Full name")
-                KabuOutlinedTextField(
-                    value = "",
-                    onTextChanged = {},
-                    placeholderText = "John Doe"
+            FormTextfield(
+                title = "Full Name",
+                text = "",
+                hintText = "John Doe",
+                onTextChanged = {}
+            )
+
+            FormTextfield(
+                title = "Email Address",
+                text = "",
+                hintText = "example@gmail.com",
+                onTextChanged = {}
+            )
+
+            FormTextfield(
+                title = "House Address",
+                text = "",
+                hintText = "House address here",
+                onTextChanged = {}
+            )
+
+            Row (
+            ){
+                RowScopeFormTextfield(
+                    title = "City",
+                    text = "",
+                    hintText = "City here",
+                    isDropdown = false,
+                    onTextChanged = {}
+                )
+                KabuSpacer(16)
+                RowScopeFormTextfield(
+                    title = "State",
+                    text = "",
+                    hintText = "Abia State",
+                    isDropdown = true,
+                    onTextChanged = {}
                 )
             }
 
@@ -57,15 +86,86 @@ fun DriverBioDataScreen() {
     }
 }
 
+@Composable
+internal fun FormTextfield(
+    title: String,
+    text: String,
+    hintText: String,
+    onTextChanged: (String) -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .background(color = Color(0x4DF1F1F1))
+            .padding(vertical = 12.dp)
+            .fillMaxWidth()
+    ) {
+        Column {
+            AnnotatedTextfieldTitle(title = title)
+            KabuOutlinedTextField(
+                value = text,
+                onTextChanged = onTextChanged,
+                placeholderText = hintText,
+                textFieldColors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = Color(0xFFF1F1F1),
+                    focusedContainerColor = Color(0xFFF1F1F1),
+                )
+            )
+        }
+    }
+}
 
 @Composable
-fun AnnotatedTextfieldTitle(title: String) {
+internal fun RowScope.RowScopeFormTextfield(
+    title: String,
+    text: String,
+    hintText: String,
+    isDropdown: Boolean,
+    onTextChanged: (String) -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .background(color = Color(0x4DF1F1F1))
+            .padding(vertical = 12.dp)
+            .weight(1f),
+//            .fillMaxWidth()
+    ) {
+        Column {
+            AnnotatedTextfieldTitle(title = title)
+            if (isDropdown) {
+                KabuOutlinedTextFieldWithTrailingIconButton(
+                    value = text,
+                    onTextChanged = onTextChanged,
+                    placeholderText = hintText,
+                    textFieldColors = TextFieldDefaults.colors(
+                        unfocusedContainerColor = Color(0xFFF1F1F1),
+                        focusedContainerColor = Color(0xFFF1F1F1),
+                    )
+                )
+            } else {
+                KabuOutlinedTextField(
+                    value = text,
+                    onTextChanged = onTextChanged,
+                    placeholderText = hintText,
+                    textFieldColors = TextFieldDefaults.colors(
+                        unfocusedContainerColor = Color(0xFFF1F1F1),
+                        focusedContainerColor = Color(0xFFF1F1F1),
+                    )
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+internal fun AnnotatedTextfieldTitle(title: String) {
     Row(
         modifier = Modifier.padding()
     ) {
-        TitleText(title, endPadding = 3)
+        TitleText(title, endPadding = 3, fontSize = 15)
         TitleText(
             "*",
+            fontSize = 15,
             color = MaterialTheme.colorScheme.error
         )
 
